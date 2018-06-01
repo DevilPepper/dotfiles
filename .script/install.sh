3505284f26e139b9c5547d7c549c72d588c9c11c
SupaStuff <StuffQilla@gmail.com>
2018-06-01 18:33:37 -0400

    added some git aliases and updated the install script

--- .script/install.sh
+++ .script/install.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Read links.txt. Expects it to be a text file with
-# dotfile in here _space_ dotfile to be symlinked.
-# \/\/ See the input redirect at done \/\/
-while read -r key value
+cd ~/dotfiles
+for f in $(stow --simulate -v * 2>&1 | grep \* | rev | cut -f1 -d' ' | rev)
 do
-    value=$(eval echo $value)
-    # do the symlink
-    echo "ln -svf $1/$key $value"
-    ln -svf $1/$key $value
-done < $1/links.txt
+    rm -rf ~/$f
+done
+stow *
 
+apm install --packages-file ~/.atom/.packages
