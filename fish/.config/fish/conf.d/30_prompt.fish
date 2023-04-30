42d6364115b82844fd9f88dd448de3faaa47415b
DevilPepper <Stuff_Git_NaoResposta@outlook.com>
2023-04-30 14:01:14 -0400

    fix: put horizontal rule in prompt and make bat 'plain'

--- fish/.config/fish/conf.d/30_prompt.fish
+++ fish/.config/fish/conf.d/30_prompt.fish
@@ -1,7 +1,14 @@
-# function starship_hr
-#     printf %"$COLUMNS"s | tr " " "_"
-# end
-
-# set -x starship_precmd_user_func "starship_hr"
+function starship_hr
+    set_color brblack
+    echo -n $(printf %"$COLUMNS"s | tr " " "_")
+    set_color normal
+end
 
 starship init fish | source
+
+functions -c fish_prompt starfish_prompt
+
+function fish_prompt
+    starship_hr
+    starfish_prompt
+end
