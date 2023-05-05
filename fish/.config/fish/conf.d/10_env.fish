afeb5c4906f36d8109de8aba7a5bf051001aa4ee
DevilPepper <Stuff_Git_NaoResposta@outlook.com>
2023-05-05 12:45:55 -0400

    feat: add fzf key bindings

--- fish/.config/fish/conf.d/10_env.fish
+++ fish/.config/fish/conf.d/10_env.fish
@@ -1,3 +1,8 @@
+set FZF_BIND "--bind shift-up:preview-page-up,shift-down:preview-page-down,alt-up:preview-top,alt-down:preview-bottom"
+set FZF_PREVIEW "--preview 'bat --color=always {} 2> /dev/null || lsd -la {}'"
+set -x FZF_DEFAULT_OPTS "$FZF_BIND $FZF_PREVIEW"
+set -x FZF_DEFAULT_COMMAND "fd --strip-cwd-prefix --hidden --exclude .git"
+
 set -x VIRTUAL_ENV_DISABLE_PROMPT true
 set -x N_PREFIX ~/.local/share/n
 
