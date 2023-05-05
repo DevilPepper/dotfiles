171570386dd8841e6cb799ff8b56acb1dfa0190f
DevilPepper <Stuff_Git_NaoResposta@outlook.com>
2023-05-05 13:56:46 -0400

    feat: add gcd (git cd)

---
+++ fish/.config/fish/conf.d/40_alias.fish
@@ -0,0 +1,7 @@
+alias beautiful='python -m json.tool'
+# alias open='xdg-open'
+alias tmux-apocalypse='tmux ls | grep : | cut -d: -f1 | xargs -n 1 -r tmux kill-session -t'
+alias comment='sed -i "s/^\"$1\"/#/" '
+
+# git cd
+alias gcd='git is-repo && cd $(git root)/$(fd --base-directory $(git root) --type d --hidden --exclude .git/ | fzf || echo) || cd $(ls -d ~/code/* | fzf)'
