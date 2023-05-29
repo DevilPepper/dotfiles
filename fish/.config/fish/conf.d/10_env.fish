1aa98d42c06a9c913536c9e09d637e9fda787201
DevilPepper <Stuff_Git_NaoResposta@outlook.com>
2023-05-29 00:51:48 -0400

    chore: update fzf and kitty config

--- fish/.config/fish/conf.d/10_env.fish
+++ fish/.config/fish/conf.d/10_env.fish
@@ -1,6 +1,6 @@
 set FZF_BIND "--bind shift-up:preview-page-up,shift-down:preview-page-down,alt-up:preview-top,alt-down:preview-bottom"
-set FZF_PREVIEW "--preview 'bat --color=always {} 2> /dev/null || lsd -la {}'"
-set -x FZF_DEFAULT_OPTS "$FZF_BIND $FZF_PREVIEW"
+set FZF_PREVIEW "--preview-window='right:75%' --preview 'bat --color=always {} 2> /dev/null || lsd -la {}'"
+set -x FZF_DEFAULT_OPTS "--multi $FZF_BIND $FZF_PREVIEW"
 set -x FZF_DEFAULT_COMMAND "fd --strip-cwd-prefix --hidden --exclude .git"
 
 set -x GPG_TTY $(tty)
