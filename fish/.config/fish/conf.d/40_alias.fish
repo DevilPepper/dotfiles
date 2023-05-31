37369a60b5f76cb08491a2a92dd503c5352eb1fb
DevilPepper <Stuff_Git_NaoResposta@outlook.com>
2023-05-31 00:27:23 -0400

    feat: add alias for generating a qrcode in the terminal

--- fish/.config/fish/conf.d/40_alias.fish
+++ fish/.config/fish/conf.d/40_alias.fish
@@ -5,3 +5,5 @@ alias comment='sed -i "s/^\"$1\"/#/" '
 
 # git cd
 alias gcd='git is-repo && cd $(git root)/$(fd --base-directory $(git root) --type d --hidden --exclude .git/ | fzf || echo) || cd $(ls -d ~/code/* | fzf)'
+
+alias qrcode='qrencode -t ansiutf8'
