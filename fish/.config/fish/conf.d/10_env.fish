d5c5d6f3085304bd421c55464c7e6a3e87148223
DevilPepper <noreply@devilpepper.anonaddy.me>
2023-09-03 23:14:33 -0400

    feat: add preview label to fzf and force lf line endings for git

--- fish/.config/fish/conf.d/10_env.fish
+++ fish/.config/fish/conf.d/10_env.fish
@@ -1,6 +1,7 @@
-set FZF_BIND "--bind shift-up:preview-page-up,shift-down:preview-page-down,alt-up:preview-top,alt-down:preview-bottom"
+set FZF_BIND_FOCUS "--bind 'focus:transform-preview-label:echo [ {} ]'"
+set FZF_BIND_KEYS "--bind shift-up:preview-page-up,shift-down:preview-page-down,alt-up:preview-top,alt-down:preview-bottom"
 set FZF_PREVIEW "--preview-window='right:75%' --preview 'bat --color=always {} 2> /dev/null || lsd -la {}'"
-set -x FZF_DEFAULT_OPTS "--multi $FZF_BIND $FZF_PREVIEW"
+set -x FZF_DEFAULT_OPTS "--multi $FZF_BIND_KEYS $FZF_BIND_FOCUS $FZF_PREVIEW"
 set -x FZF_DEFAULT_COMMAND "fd --strip-cwd-prefix --hidden --exclude .git"
 
 set -x GPG_TTY $(tty)
