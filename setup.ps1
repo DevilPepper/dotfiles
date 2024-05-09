5143c3857f3b82a27d54c95f24157a44976a5bc9
DevilPepper <noreply@devilpepper.anonaddy.me>
2024-05-08 22:37:58 -0400

    feat: clean up navigation pane

--- setup.ps1
+++ setup.ps1
@@ -11,6 +11,33 @@ $winConfig = Force-Resolve-Path -Path "~/AppData/Local"
 $dotfilesPath = "$PSScriptRoot"
 $ogDotfilesPath = Force-Resolve-Path -Path "${winConfig}/dotfiles"
 
+$com = New-Object -ComObject Shell.Application
+
+function main {
+  # There has to be a reason this isn't a symlink...
+  cp "$dotfilesPath/Microsoft.PowerShell_profile.ps1" $profile -WhatIf:$WhatIf
+
+  Get-ChildItem -Path "${dotfilesPath}/ini/desktop" | foreach { Set-FolderInfo -ini $_.FullName }
+  Fix-QuickAccess
+
+  Git-Clone -repo "https://github.com/DevilPepper/dotfiles.git" -destination "$ogDotfilesPath"
+  Git-Clone -repo "https://github.com/DevilPepper/nvim-plugins.git" -destination "${winConfig}/nvim-data/site/pack" -recurse
+
+  Get-ChildItem -Path "${dotfilesPath}/reg" | foreach { Reg-Edit -reg $_.FullName }
+
+  Stow -source "${dotfilesPath}/AppData" -destination "~/AppData"
+  Stow -source "${dotfilesPath}/.config" -destination "${xdgConfig}"
+
+  Stow -source "${ogDotfilesPath}/ascii/.local/share" -destination "${xdgData}"
+  Stow -source "${ogDotfilesPath}/git/.config" -destination "${xdgConfig}"
+  Stow -source "${ogDotfilesPath}/nvim/.config" -destination "${winConfig}"
+
+  # Stupid defaults give us dirty worktree.
+  # Pretty destructive workaround... Shouldn't be touching this directory anyway
+  Git-Restore -destination "${ogDotfilesPath}"
+  Git-Restore -destination "${winConfig}/nvim-data/site/pack" -recurse
+}
+
 function Stow {
   param(
     [string]$source,
@@ -29,7 +56,7 @@ Get-ChildItem -Path "${src}" -Recurse `
     if (Test-Path -Path $destinationFile) {
       Remove-Item -Force $destinationFile -WhatIf:$WhatIf
     }
-    New-Item -ItemType "directory" -Path $destinationDir -Force > $null
+    New-Item -ItemType "directory" -Path $destinationDir -ErrorAction SilentlyContinue -WhatIf:$WhatIf > $null
     New-Item -ItemType SymbolicLink -Target $_.FullName -Path $destinationFile -WhatIf:$WhatIf > $null
   }
 }
@@ -84,14 +111,10 @@ function Git-Restore {
   }
 }
 
-# There has to be a reason this isn't a symlink...
-cp "$dotfilesPath/Microsoft.PowerShell_profile.ps1" $profile -WhatIf:$WhatIf
-
-Git-Clone -repo "https://github.com/DevilPepper/dotfiles.git" -destination "$ogDotfilesPath"
-Git-Clone -repo "https://github.com/DevilPepper/nvim-plugins.git" -destination "${winConfig}/nvim-data/site/pack" -recurse
-
-Get-ChildItem -Path "${dotfilesPath}/reg" | foreach {
-  $reg = $_.FullName
+function Reg-Edit {
+  param(
+    [string]$reg
+  )
   if ($WhatIf) {
     echo "What if: reg import ${reg}"
   } else {
@@ -99,14 +122,54 @@ Get-ChildItem -Path "${dotfilesPath}/reg" | foreach {
   }
 }
 
-Stow -source "${dotfilesPath}/AppData" -destination "~/AppData"
-Stow -source "${dotfilesPath}/.config" -destination "${xdgConfig}"
+function Set-FolderInfo {
+  param(
+    [string]$ini
+  )
+  $dest = (Get-Content $ini -First 1).SubString(2)
+  $dest = Force-Resolve-Path -Path "${dest}"
 
-Stow -source "${ogDotfilesPath}/ascii/.local/share" -destination "${xdgData}"
-Stow -source "${ogDotfilesPath}/git/.config" -destination "${xdgConfig}"
-Stow -source "${ogDotfilesPath}/nvim/.config" -destination "${winConfig}"
+  New-Item -ItemType "directory" -Path $dest -ErrorAction SilentlyContinue -WhatIf:$WhatIf > $null
+  Remove-Item "${dest}/desktop.ini" -Force -WhatIf:$WhatIf
+  Copy-Item $ini -Destination "${dest}/desktop.ini" -WhatIf:$WhatIf
+  Set-ItemProperty "${dest}/desktop.ini" -Name Attributes -Value "Archive,System,Hidden" -WhatIf:$WhatIf
+  Set-ItemProperty "${dest}" -Name Attributes -Value "ReadOnly" -WhatIf:$WhatIf
+}
 
-# Stupid defaults give us dirty worktree.
-# Pretty destructive workaround... Shouldn't be touching this directory anyway
-Git-Restore -destination "${ogDotfilesPath}"
-Git-Restore -destination "${winConfig}/nvim-data/site/pack" -recurse
+function Fix-QuickAccess {
+  $folderPins = @(
+    "~\home"
+    "~\AppData"
+    "~\Downloads"
+    "~\home\Documents"
+    "~\Pictures"
+    "~\Music"
+    "~\Videos"
+    "C:\Program Files (x86)\Steam\steamapps\Steam"
+  ) | foreach { (Force-Resolve-Path -Path $_).Path }
+
+  if ($WhatIf) {
+    echo "WhatIf: Pinning like this:"
+    echo "📌 This PC"
+    $folderPins | foreach { echo "📌 $_" }
+    echo "📌 Linux"
+    echo "📌 Network"
+  } else {
+    $com.Namespace("shell:::{679f85cb-0220-4080-b29b-5540cc05aab6}").Items() | foreach { $_.InvokeVerb("unpinfromhome") }
+
+    # "": Desktop
+    # "\\wsl.localhost": Linux
+    # 0x12: Network
+    # For more: https://learn.microsoft.com/en-us/windows/win32/api/shldisp/ne-shldisp-shellspecialfolderconstants
+    $namespaces = @("") `
+      + $folderPins `
+      + @(
+          "\\wsl.localhost",
+          0x12
+        )
+
+    $namespaces | foreach { $com.Namespace($_).Self.InvokeVerb("pintohome") }
+  }
+}
+
+main
