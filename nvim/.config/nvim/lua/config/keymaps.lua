df8b72756bfcf5a14782682cf62ed47176da2855
DevilPepper <noreply@devilpepper.anonaddy.me>
2024-05-24 13:34:07 -0400

    feat: add sg.nvim

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,5 @@
 local telescope_builtin = require('telescope.builtin')
+local sg_extensions = require('sg.extensions.telescope')
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
@@ -6,3 +7,5 @@ vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', telescope_builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, {})
+
+vim.keymap.set('n', '<leader>ss', sg_extensions.fuzzy_search_results, {})
