fd91e3df96c48f59292b2be9ce145443fa31f247
DevilPepper <noreply@devilpepper.anonaddy.me>
2024-06-21 21:19:21 -0400

    feat: add interactive script to fetch plugins and completions

---
+++ finish-setup.sh
@@ -0,0 +1,116 @@
+#!/usr/bin/env bash
+set -e
+
+code_path=~/code
+zsh_completions_path=~/.local/share/zsh/completions
+bash_completions_path=~/.local/share/bash-completion/completions
+
+function main() {
+    repos=($(gum choose "nvim-plugins" "zsh-plugins" --header "What repos do you want to fetch?" --no-limit))
+
+    use_ssh=false
+    if [[ ${#repos[@]} -ne 0 ]]; then
+        gum confirm "Use ssh?" && use_ssh=true || use_ssh=false
+    fi
+
+    completions=($(gum choose "git" "gcloud" --header "What completions do you need?" --no-limit))
+
+    # spinner "Cloning repos..." symlink_repos "${use_ssh}" "${repos[@]}"
+    # spinner "Setting up completions..." get_completions "${completions[@]}"
+
+    symlink_repos "${use_ssh}" "${repos[@]}"
+    get_completions "${completions[@]}"
+}
+
+function get_completions() {
+    local completions=("$@")
+
+    if [[ ${#completions[@]} -ne 0 ]]; then
+        mkdir -p $bash_completions_path
+        mkdir -p $zsh_completions_path
+
+        if $(array_contains_string "git" "${completions[@]}"); then
+            repo_raw="https://raw.githubusercontent.com/git/git/master"
+            curl -o $bash_completions_path/git "$repo_raw/contrib/completion/git-completion.bash"
+            curl -o $zsh_completions_path/_git "$repo_raw/contrib/completion/git-completion.zsh"
+        fi
+
+        if $(array_contains_string "gcloud" "${completions[@]}"); then
+            echo "(gcloud completion) Not yet implemented..." 1>&2
+        fi
+
+        rm -f "${ZDOTDIR:-$HOME/.config/zsh}/.zcompdump"
+    fi
+}
+
+function symlink_repos() {
+    local use_ssh="$1"
+    shift
+    local repos=("$@")
+
+    if $(array_contains_string "zsh-plugins" "${repos[@]}"); then
+        symlink_repo "DevilPepper/zsh-plugins" ~/.local/share/zsh/plugins "${use_ssh}"
+    fi
+
+    # Do this one last because it's big
+    if $(array_contains_string "nvim-plugins" "${repos[@]}"); then
+        symlink_repo "DevilPepper/nvim-plugins" ~/.local/share/nvim/site/pack "${use_ssh}"
+        if [ ! -z $(which nvim) ]; then
+            nvim -s <(echo ":helptags ALL" && echo ":q")
+        else
+            echo "Didn't generate helptags because nvim is not installed" 1>&2
+        fi
+    fi
+}
+
+function symlink_repo() {
+    local owner_repo="$1"
+    local link_path="$2"
+    local use_ssh="$3"
+
+    repo_path="$code_path/$(basename $owner_repo)"
+    link_parent=$(dirname "$link_path")
+    repo_url="https://github.com/${owner_repo}.git"
+    if [ "$use_ssh" = true ]; then
+        repo_url="git@github.com:${owner_repo}.git"
+    fi
+
+    mkdir -p $link_parent
+    rm -rf $repo_path
+
+    git clone --recurse-submodules $repo_url $repo_path
+    ln -sf $repo_path $link_path
+}
+
+function array_contains_string() {
+    local search_string="$1"
+    shift
+    local arr=("$@")
+    local found=false
+
+    for item in "${arr[@]}"; do
+        if [[ "$item" == "$search_string" ]]; then
+            found=true
+            break
+        fi
+    done
+
+    echo $found
+}
+
+# function spinner() {
+#     local title="$1"
+#     local func="$2"
+#     shift 2
+
+#     set +o monitor
+#     gum spin --title="$title" -- sleep infinity &
+#     spinner_pid=$!
+
+#     $func "$@"
+
+#     kill $spinner_pid
+#     set -o monitor
+# }
+
+main
--- zsh/.config/zsh/conf.d/99_highlighting.zsh
+++ zsh/.config/zsh/conf.d/99_highlighting.zsh
@@ -1,70 +1,74 @@
 # How to tweak: https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/docs/highlighters.md
-source ~/.local/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+zsh_syntax_highlighting_path=~/.local/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
-ZSH_HIGHLIGHT_HIGHLIGHTERS+=(brackets)
-typeset -A ZSH_HIGHLIGHT_STYLES
+if [ -e $zsh_syntax_highlighting_path ]; then
+    source $zsh_syntax_highlighting_path
+    ZSH_HIGHLIGHT_HIGHLIGHTERS+=(brackets)
 
-########
-# main #
-########
-ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=red'
-ZSH_HIGHLIGHT_STYLES[reserved-word]='fg=magenta'
-ZSH_HIGHLIGHT_STYLES[alias]='fg=blue,bold'
-ZSH_HIGHLIGHT_STYLES[suffix-alias]='fg=blue,underline'
-ZSH_HIGHLIGHT_STYLES[global-alias]='fg=blue,bold'
-ZSH_HIGHLIGHT_STYLES[builtin]='fg=blue'
-ZSH_HIGHLIGHT_STYLES[function]='fg=cyan'
-ZSH_HIGHLIGHT_STYLES[command]='fg=blue'
-ZSH_HIGHLIGHT_STYLES[precommand]='fg=yellow'
-ZSH_HIGHLIGHT_STYLES[commandseparator]='fg=black,bold'
-# ZSH_HIGHLIGHT_STYLES[hashed-command]=''
-# ZSH_HIGHLIGHT_STYLES[autodirectory]=''
-ZSH_HIGHLIGHT_STYLES[path]='fg=cyan,underline'
-# ZSH_HIGHLIGHT_STYLES[path_pathseparator]=''
-ZSH_HIGHLIGHT_STYLES[path_prefix]='fg=cyan,underline'
-# ZSH_HIGHLIGHT_STYLES[path_prefix_pathseparator]=''
-ZSH_HIGHLIGHT_STYLES[globbing]='fg=yellow,bold'
-ZSH_HIGHLIGHT_STYLES[history-expansion]='fg=cyan'
-# ZSH_HIGHLIGHT_STYLES[command-substitution]=''
-# ZSH_HIGHLIGHT_STYLES[command-substitution-unquoted]=''
-# ZSH_HIGHLIGHT_STYLES[command-substitution-quoted]=''
-ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter]='fg=black,bold'
-# ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter-unquoted]=''
-# ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter-quoted]=''
-# ZSH_HIGHLIGHT_STYLES[process-substitution]=''
-ZSH_HIGHLIGHT_STYLES[process-substitution-delimiter]='fg=black,bold'
-ZSH_HIGHLIGHT_STYLES[arithmetic-expansion]='fg=yellow'
-ZSH_HIGHLIGHT_STYLES[single-hyphen-option]='fg=cyan'
-ZSH_HIGHLIGHT_STYLES[double-hyphen-option]='fg=cyan'
-# ZSH_HIGHLIGHT_STYLES[back-quoted-argument]=''
-ZSH_HIGHLIGHT_STYLES[back-quoted-argument-unclosed]='fg=red'
-ZSH_HIGHLIGHT_STYLES[back-quoted-argument-delimiter]='fg=black,bold'
-ZSH_HIGHLIGHT_STYLES[single-quoted-argument]='fg=yellow'
-ZSH_HIGHLIGHT_STYLES[single-quoted-argument-unclosed]='fg=red'
-ZSH_HIGHLIGHT_STYLES[double-quoted-argument]='fg=yellow'
-ZSH_HIGHLIGHT_STYLES[double-quoted-argument-unclosed]='fg=red'
-# ZSH_HIGHLIGHT_STYLES[dollar-quoted-argument]=''
-# ZSH_HIGHLIGHT_STYLES[dollar-quoted-argument-unclosed]=''
-# ZSH_HIGHLIGHT_STYLES[rc-quote]=''
-ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]='fg=white'
-ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]='fg=black,bold'
-# ZSH_HIGHLIGHT_STYLES[back-dollar-quoted-argument]=''
-# ZSH_HIGHLIGHT_STYLES[assign]=''
-ZSH_HIGHLIGHT_STYLES[redirection]='fg=green'
-ZSH_HIGHLIGHT_STYLES[comment]='fg=black,bold'
-# ZSH_HIGHLIGHT_STYLES[elided-parameter]=''
-ZSH_HIGHLIGHT_STYLES[named-fd]='fg=magenta'
-ZSH_HIGHLIGHT_STYLES[numeric-fd]='fg=magenta'
-ZSH_HIGHLIGHT_STYLES[arg0]='fg=white,bold'
-ZSH_HIGHLIGHT_STYLES[default]='fg=white'
+    typeset -A ZSH_HIGHLIGHT_STYLES
+
+    ########
+    # main #
+    ########
+    ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=red'
+    ZSH_HIGHLIGHT_STYLES[reserved-word]='fg=magenta'
+    ZSH_HIGHLIGHT_STYLES[alias]='fg=blue,bold'
+    ZSH_HIGHLIGHT_STYLES[suffix-alias]='fg=blue,underline'
+    ZSH_HIGHLIGHT_STYLES[global-alias]='fg=blue,bold'
+    ZSH_HIGHLIGHT_STYLES[builtin]='fg=blue'
+    ZSH_HIGHLIGHT_STYLES[function]='fg=cyan'
+    ZSH_HIGHLIGHT_STYLES[command]='fg=blue'
+    ZSH_HIGHLIGHT_STYLES[precommand]='fg=yellow'
+    ZSH_HIGHLIGHT_STYLES[commandseparator]='fg=black,bold'
+    # ZSH_HIGHLIGHT_STYLES[hashed-command]=''
+    # ZSH_HIGHLIGHT_STYLES[autodirectory]=''
+    ZSH_HIGHLIGHT_STYLES[path]='fg=cyan,underline'
+    # ZSH_HIGHLIGHT_STYLES[path_pathseparator]=''
+    ZSH_HIGHLIGHT_STYLES[path_prefix]='fg=cyan,underline'
+    # ZSH_HIGHLIGHT_STYLES[path_prefix_pathseparator]=''
+    ZSH_HIGHLIGHT_STYLES[globbing]='fg=yellow,bold'
+    ZSH_HIGHLIGHT_STYLES[history-expansion]='fg=cyan'
+    # ZSH_HIGHLIGHT_STYLES[command-substitution]=''
+    # ZSH_HIGHLIGHT_STYLES[command-substitution-unquoted]=''
+    # ZSH_HIGHLIGHT_STYLES[command-substitution-quoted]=''
+    ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter]='fg=black,bold'
+    # ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter-unquoted]=''
+    # ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter-quoted]=''
+    # ZSH_HIGHLIGHT_STYLES[process-substitution]=''
+    ZSH_HIGHLIGHT_STYLES[process-substitution-delimiter]='fg=black,bold'
+    ZSH_HIGHLIGHT_STYLES[arithmetic-expansion]='fg=yellow'
+    ZSH_HIGHLIGHT_STYLES[single-hyphen-option]='fg=cyan'
+    ZSH_HIGHLIGHT_STYLES[double-hyphen-option]='fg=cyan'
+    # ZSH_HIGHLIGHT_STYLES[back-quoted-argument]=''
+    ZSH_HIGHLIGHT_STYLES[back-quoted-argument-unclosed]='fg=red'
+    ZSH_HIGHLIGHT_STYLES[back-quoted-argument-delimiter]='fg=black,bold'
+    ZSH_HIGHLIGHT_STYLES[single-quoted-argument]='fg=yellow'
+    ZSH_HIGHLIGHT_STYLES[single-quoted-argument-unclosed]='fg=red'
+    ZSH_HIGHLIGHT_STYLES[double-quoted-argument]='fg=yellow'
+    ZSH_HIGHLIGHT_STYLES[double-quoted-argument-unclosed]='fg=red'
+    # ZSH_HIGHLIGHT_STYLES[dollar-quoted-argument]=''
+    # ZSH_HIGHLIGHT_STYLES[dollar-quoted-argument-unclosed]=''
+    # ZSH_HIGHLIGHT_STYLES[rc-quote]=''
+    ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]='fg=white'
+    ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]='fg=black,bold'
+    # ZSH_HIGHLIGHT_STYLES[back-dollar-quoted-argument]=''
+    # ZSH_HIGHLIGHT_STYLES[assign]=''
+    ZSH_HIGHLIGHT_STYLES[redirection]='fg=green'
+    ZSH_HIGHLIGHT_STYLES[comment]='fg=black,bold'
+    # ZSH_HIGHLIGHT_STYLES[elided-parameter]=''
+    ZSH_HIGHLIGHT_STYLES[named-fd]='fg=magenta'
+    ZSH_HIGHLIGHT_STYLES[numeric-fd]='fg=magenta'
+    ZSH_HIGHLIGHT_STYLES[arg0]='fg=white,bold'
+    ZSH_HIGHLIGHT_STYLES[default]='fg=white'
 
 
-############
-# brackets #
-############
-ZSH_HIGHLIGHT_STYLES[bracket-error]='fg=red'
-ZSH_HIGHLIGHT_STYLES[cursor-matchingbracket]='underline'
-ZSH_HIGHLIGHT_STYLES[bracket-level-1]='fg=#656c72'
-ZSH_HIGHLIGHT_STYLES[bracket-level-2]='fg=#767573'
-ZSH_HIGHLIGHT_STYLES[bracket-level-3]='fg=#84898d'
-ZSH_HIGHLIGHT_STYLES[bracket-level-4]='fg=#949492'
+    ############
+    # brackets #
+    ############
+    ZSH_HIGHLIGHT_STYLES[bracket-error]='fg=red'
+    ZSH_HIGHLIGHT_STYLES[cursor-matchingbracket]='underline'
+    ZSH_HIGHLIGHT_STYLES[bracket-level-1]='fg=#656c72'
+    ZSH_HIGHLIGHT_STYLES[bracket-level-2]='fg=#767573'
+    ZSH_HIGHLIGHT_STYLES[bracket-level-3]='fg=#84898d'
+    ZSH_HIGHLIGHT_STYLES[bracket-level-4]='fg=#949492'
+fi
