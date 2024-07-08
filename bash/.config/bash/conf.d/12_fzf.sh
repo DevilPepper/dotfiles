99738e12aa87c4eb4357f48f141e2e19a71578b4
DevilPepper <noreply@devilpepper.anonaddy.me>
2024-07-08 19:46:14 -0400

    fix: hitting Esc in fzf is not SIGINT

--- bash/.config/bash/conf.d/12_fzf.sh
+++ bash/.config/bash/conf.d/12_fzf.sh
@@ -1,5 +1,7 @@
 FZF_BIND_FOCUS="--bind 'focus:transform-preview-label:echo [ {} ]'"
 FZF_BIND_KEYS="--bind shift-up:preview-page-up,shift-down:preview-page-down,alt-up:preview-top,alt-down:preview-bottom"
+FZF_BIND_BECOME="--bind 'esc:become:'"
 FZF_PREVIEW="--preview-window='right:75%' --preview 'bat --color=always {} 2> /dev/null || lsd -la {}'"
-export FZF_DEFAULT_OPTS="--multi $FZF_BIND_KEYS $FZF_BIND_FOCUS $FZF_PREVIEW"
+
+export FZF_DEFAULT_OPTS="--multi $FZF_BIND_KEYS $FZF_BIND_FOCUS $FZF_PREVIEW $FZF_BIND_BECOME"
 export FZF_DEFAULT_COMMAND="fd --strip-cwd-prefix --hidden --exclude .git"
