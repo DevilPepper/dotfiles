eb4cd569c2a3b87e26a269c3f5a3319c7877c1c0
DevilPepper <noreply@devilpepper.anonaddy.me>
2024-11-18 22:40:54 -0500

    fix: remove homebrew packages that should be provided by Xcode and bump local binary priority

--- bash/.config/bash/conf.d/10_env.sh
+++ bash/.config/bash/conf.d/10_env.sh
@@ -9,9 +9,9 @@ export VIRTUAL_ENV_DISABLE_PROMPT=true
 
 export HOSTNAME
 
-# Mac doesn't include this in the default PATH
+# Mac doesn't include these in the default PATH and homebrew x86_64 installs in /usr/local/bin
 PATH=$PATH:/usr/local/bin
-PATH=$PATH:/opt/homebrew/bin
-PATH=$PATH:~/.local/bin
+PATH=/opt/homebrew/bin:$PATH
+PATH=~/.local/bin:$PATH
 PATH=$PATH:./node_modules/.bin
 export PATH
