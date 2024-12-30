3d10a982ffaf2b2f1fe8adb1d373d8b5eba15153
DevilPepper <noreply@devilpepper.anonaddy.me>
2024-12-30 16:03:10 -0500

    fix: add PYTHON_HISTORY variable and only do the history hack if <=3.12

--- python/.config/python/startup.py
+++ python/.config/python/startup.py
@@ -8,25 +8,25 @@ from datetime import datetime
 sys.ps1 = "\001\033[32m\002\001\033[0m\002 "
 sys.ps2 = "├   "
 
-history = os.path.join(
-    (
-        os.environ.get('XDG_STATE_HOME') or
-        os.path.expanduser('~/.local/state')
-    ),
-    'history.py'
-)
+if sys.version_info[0] <= 3 and sys.version_info[1] <= 12:
+  history = os.path.join(
+      (
+          os.environ.get('XDG_STATE_HOME') or
+          os.path.expanduser('~/.local/state')
+      ),
+      'history.py'
+  )
 
-try:
-    readline.read_history_file(history)
-except FileNotFoundError:
-    readline.add_history(f'# History created at {datetime.now().isoformat()}')
-except OSError as e:
-    raise OSError(f'{history} cannot be a directory') from e
+  try:
+      readline.read_history_file(history)
+  except FileNotFoundError:
+      readline.add_history(f'# History created at {datetime.now().isoformat()}')
+  except OSError as e:
+      raise OSError(f'{history} cannot be a directory') from e
 
-def write_history():
-    try:
-        readline.write_history_file(history)
-    except OSError:
-        pass
-
-atexit.register(write_history)
+  @atexit.register
+  def write_history():
+      try:
+          readline.write_history_file(history)
+      except OSError:
+          pass
