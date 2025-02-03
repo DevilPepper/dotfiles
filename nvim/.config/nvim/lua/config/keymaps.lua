9ef48ec97900443dd2c5887d83dc022d60c7032f
DevilPepper <noreply@devilpepper.anonaddy.me>
2025-02-03 00:46:03 -0500

    feat: add Github Copilot to NeoVim

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,5 @@ vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', telescope_builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, {})
+
+vim.keymap.set('i', '<C-i>', '<Plug>(copilot-suggest)')
