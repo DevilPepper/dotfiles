4ff18df1c1e6f8f2b92211b77321c4fe19d0b1cc
DevilPepper <noreply@devilpepper.anonaddy.me>
2026-03-04 18:39:39 -0500

    feat: add FAHHH! to non-zero exit codes

--- bash/.config/bash/conf.d/30_prompt.sh
+++ bash/.config/bash/conf.d/30_prompt.sh
@@ -1,5 +1,11 @@
 function starship_hr() {
-    if [[ $? -ne 0 ]]; then
+    if [[ $? -ne 0 && $? -ne 130 ]]; then
+        FAHHH=~/.local/share/fahhh.wav
+        if [[ "$(uname)" == "Darwin" ]]; then
+            (afplay $FAHHH &)
+        elif command -v paplay &>/dev/null; then
+            (paplay $FAHHH &)
+        fi
         tput setaf 1
     else
         tput setaf 0
