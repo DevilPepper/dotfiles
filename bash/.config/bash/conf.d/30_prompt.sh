a077f788f34404d05ab938dcd4438f5a01cc15cf
DevilPepper <noreply@devilpepper.anonaddy.me>
2026-06-11 21:55:50 -0400

    Revert "feat: add FAHHH! to non-zero exit codes"
    This reverts commit 4ff18df1c1e6f8f2b92211b77321c4fe19d0b1cc.

--- bash/.config/bash/conf.d/30_prompt.sh
+++ bash/.config/bash/conf.d/30_prompt.sh
@@ -1,11 +1,5 @@
 function starship_hr() {
-    if [[ $? -ne 0 && $? -ne 130 ]]; then
-        FAHHH=~/.local/share/fahhh.wav
-        if [[ "$(uname)" == "Darwin" ]]; then
-            (afplay $FAHHH &)
-        elif command -v paplay &>/dev/null; then
-            (paplay $FAHHH &)
-        fi
+    if [[ $? -ne 0 ]]; then
         tput setaf 1
     else
         tput setaf 0
